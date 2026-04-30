ab9176c627970fe5f890d89b7c5721c1ee78265b
lebomofube1818-sys <lebomofube1818-sys@gmail.com>
2026-04-30 15:02:29 +0200

    Task 3: printTeacher function with interface

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -30,3 +30,17 @@ const director1: Directors = {
 };
 
 console.log(director1);
+
+
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = (
+  firstName: string,
+  lastName: string
+): string => {
+  return `${firstName.charAt(0)}. ${lastName}`;
+};
+
+console.log(printTeacher('John', 'Doe'));
