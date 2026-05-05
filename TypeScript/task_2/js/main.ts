b247db916d221e7d23f24d40c9ec71e420de8ab5
lebomofube1818-sys <lebomofube1818-sys@gmail.com>
2026-05-05 12:39:24 +0200

    Task 6: Type predicate and executeWork

--- TypeScript/task_2/js/main.ts
+++ TypeScript/task_2/js/main.ts
@@ -50,7 +50,22 @@ function createEmployee(salary: number | string): Director | Teacher {
   return new Director();
 }
 
+// Type predicate
+function isDirector(employee: Director | Teacher): employee is Director {
+  return employee instanceof Director;
+}
+
+// Function executeWork
+function executeWork(employee: Director | Teacher): string {
+  if (isDirector(employee)) {
+    return employee.workDirectorTasks();
+  }
+  return employee.workTeacherTasks();
+}
+
 // Test cases (VERY IMPORTANT)
-console.log(createEmployee(200));    // Teacher
-console.log(createEmployee(1000));   // Director
-console.log(createEmployee('$500')); // Director
+console.log(createEmployee(200));
+console.log(createEmployee(1000));
+console.log(createEmployee('$500'));
+console.log(executeWork(createEmployee(200)));
+console.log(executeWork(createEmployee(1000)));
